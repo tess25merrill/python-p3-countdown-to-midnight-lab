9d550d3d645d60f4b878e4cbd76a2b013c6bfd68
Tess <tess.merrill25@gmail.com>
2023-08-21 18:46:32 -0600

    countdown lab

--- lib/countdown.py
+++ lib/countdown.py
@@ -1,1 +1,17 @@
 # your code goes here!
+import time
+
+def countdown(int_arg):
+    while int_arg >= 1:
+        print(f'{int_arg} SECOND(S)!')
+        int_arg -= 1
+        if int_arg == 0:
+            print("HAPPY NEW YEAR!")
+
+def countdown_with_sleep(int):
+    while int >= 1:
+        print(f'{int} SECOND(S)!')
+        time.sleep(1)
+        int -= 1
+        if int == 0:
+            print("HAPPY NEW YEAR!")
